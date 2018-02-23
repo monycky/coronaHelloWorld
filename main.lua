09fc5997dad0d0a460bd767dc155e75d00aca9da
monycky <monyckyfrazao@gmail.com>
2018-02-23 15:41:42 -0300

    Creating a first element move

--- main.lua
+++ main.lua
@@ -5,3 +5,17 @@
 -----------------------------------------------------------------------------------------
 
 -- Your code here
+
+--load image
+local pig = display.newImage("pig.png")
+
+--element position
+pig.x = display.contentCenterX
+pig.y = display.contentCenterY
+
+
+--adding physics
+local physics = require("physics")
+physics.start( )
+
+physics.addBody(pig, "dynamic", {radius = 100})
