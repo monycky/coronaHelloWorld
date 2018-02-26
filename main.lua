16146acae902bee5148988d93c7de0c499832a9b
monycky <monyckyfrazao@gmail.com>
2018-02-26 11:12:51 -0300

    Add a rabbit's jump physics

--- main.lua
+++ main.lua
@@ -7,15 +7,24 @@
 -- Your code here
 
 --load image
-local pig = display.newImage("pig.png")
+local background = display.newImageRect("background.png", 450, 1050)
+background.x = display.contentCenterX
+background.y = display.contentCenterY
 
+local rabbit = display.newImage("rabbit.png")
+local ground = display.newImage("ground.png")
 --element position
-pig.x = display.contentCenterX
-pig.y = display.contentCenterY
+rabbit.x = display.contentCenterX
+rabbit.y = display.contentCenterY
 
+ground.x = display.contentCenterX
+ground.y = display.contentHeight -1
 
 --adding physics
 local physics = require("physics")
 physics.start( )
 
-physics.addBody(pig, "dynamic", {radius = 100})
+physics.addBody(rabbit, "dynamic", {density = 0, friction = 1, bounce = 1})
+physics.addBody(ground, "static", {friction = 1})
+
+
